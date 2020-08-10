d45e7104d467a793a1f58ccbc6b8ccd227ee14ba
Luciano Donati <me@lucianodonati.com>
2020-08-10 19:24:54 -0300

    Codigo ScriptableObjects de paleta de colores ejemplo

---
+++ PaletaDeColores/Assets/Scripts/PaletaDeColores/ColorAleatorioDePaleta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorAleatorioDePaleta : MonoBehaviour
+{
+    public PaletaDeColores laPaletaActual;
+
+    private void Awake()
+    {
+        GetComponent<Image>().color = laPaletaActual.DameColorAleatorio();
+    }
+}
---
+++ PaletaDeColores/Assets/Scripts/PaletaDeColores/PaletaDeColores.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Paleta", menuName = "ScriptableObjects/PaletaDeColores")]
+public class PaletaDeColores : ScriptableObject
+{
+    public Color[] paleta;
+
+    [Range(1, 10)]
+    public int tiempoDeRecarga = 5;
+
+    public Color DameColorAleatorio()
+    {
+        return paleta[Random.Range(0, paleta.Length)];
+    }
+}
